82cf9f09340cc6a55bbfb648326af8c62e2bac51
Kevin Heard <kev@kevinheard.com>
2020-04-23 14:01:12 +1000

    initial setup for social ape plus outputting screams form firebase api

---
+++ src/components/Scream.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    display: 'flex'
+  }
+};
+
+class Scream extends Component {
+  render() {
+    const { classes, scream: { body, createdAt, userImage, userHandle, screamId, likeCount, commentCunt } } = this.props; 
+    return (
+      <Card>
+        <CardMedia
+          image={userImage}
+          title="Profile i mage"
+        />
+        <CardContent>
+          <Typography variant="h5">{userHandle}</Typography>
+          <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+          <Typography variant="body1">{body}</Typography>
+        </CardContent>
+      </Card>
+    )
+  }
+}
+
+export default withStyles(styles)(Scream);
---
+++ src/pages/home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+import Scream from '../components/Scream'
+
+class home extends Component {
+  state = {
+    screams: null
+  }
+
+  componentDidMount() {
+    axios.get('/screams')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <Scream scream={scream} /> )
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+         <Grid item sm={4} xs={12}>
+           Profile
+         </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default home
