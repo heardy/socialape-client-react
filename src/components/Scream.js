da0577f6bb59f56728f02db9aefecb475b435970
Kevin Heard <kevin.heard@travelctm.com>
2020-04-25 10:09:48 +1000

    additional styles for screams and fix errors

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
+import { Link } from 'react-router-dom';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,7 +9,15 @@ import Typography from '@material-ui/core/Typography';
 
 const styles = {
   card: {
-    display: 'flex'
+    display: 'flex',
+    marginBottom: 20
+  },
+  image: {
+    minWidth: 200,
+    objectFit: 'cover'
+  },
+  content: {
+    padding: 25
   }
 };
 
@@ -16,13 +25,21 @@ class Scream extends Component {
   render() {
     const { classes, scream: { body, createdAt, userImage, userHandle, screamId, likeCount, commentCunt } } = this.props; 
     return (
-      <Card>
+      <Card className={classes.card}>
         <CardMedia
+          className={classes.image}
           image={userImage}
           title="Profile i mage"
         />
-        <CardContent>
-          <Typography variant="h5">{userHandle}</Typography>
+        <CardContent className={classes.content}>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={`/users/${userHandle}`}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
           <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
