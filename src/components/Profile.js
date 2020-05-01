c33df8feb305f372343d5c2fce083974f4cc13a6
Kevin Heard <kevin.heard@travelctm.com>
2020-05-01 17:15:42 +1000

    logout and edit profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,28 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
+import EditDetails from './EditDetails';
 
 // MUI stuff
 import Button from '@material-ui/core/Button';
 import MuiLink from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 
 // Icons
 import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
+import EditIcon from '@material-ui/icons/Edit';
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
+
+// Redux
+import { connect } from 'react-redux';
+import { logoutUser, uploadImage } from '../redux/actions/userActions';
 
 const styles = (theme) => ({
   paper: {
@@ -65,6 +73,22 @@ const styles = (theme) => ({
 });
 
 class Profile extends Component {
+  handleImageChange = (event) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append('image', image, image.name);
+    this.props.uploadImage(formData);
+  }
+
+  handleEditPicture = () => {
+    const fileInput = document.getElementById('imageInput');
+    fileInput.click();
+  }
+
+  handleLogout = () => {
+    this.props.logoutUser();
+  }
+
   render() {
     const {
       classes,
@@ -80,6 +104,12 @@ class Profile extends Component {
         <div className={classes.profile}>
           <div className="image-wrapper">
             <img src={imageUrl} alt="profile" className="profile-image" />
+            <input type="file" id="imageInput" hidden="hidden" onChange={this.handleImageChange} />
+            <Tooltip title="Edit profile picture" placement="top">
+              <IconButton onClick={this.handleEditPicture} className="button">
+                <EditIcon color="primary" />
+              </IconButton>
+            </Tooltip>
           </div>
           <hr />
           <div className="profile-details">
@@ -107,6 +137,12 @@ class Profile extends Component {
             <CalendarToday color="primary" />{' '}
             <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
           </div>
+          <Tooltip title="Logout" placement="top">
+            <IconButton onClick={this.handleLogout}>
+              <KeyboardReturn color="primary" />
+            </IconButton>
+          </Tooltip>
+          <EditDetails />
         </div>
       </Paper>
     ) : (
@@ -133,9 +169,13 @@ const mapStateToProps = (state) => ({
   user: state.user
 });
 
+const mapActionsToProps = { logoutUser, uploadImage };
+
 Profile.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired
 };
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile));
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile));
--- src/pages/home.js
+++ src/pages/home.js
@@ -13,7 +13,6 @@ class home extends Component {
   componentDidMount() {
     axios.get('/screams')
       .then(res => {
-        console.log(res.data);
         this.setState({
           screams: res.data
         });
@@ -32,9 +31,9 @@ class home extends Component {
         <Grid item sm={8} xs={12}>
           {recentScreamsMarkup}
         </Grid>
-         <Grid item sm={4} xs={12}>
-           <Profile />
-         </Grid>
+        <Grid item sm={4} xs={12}>
+          <Profile />
+        </Grid>
       </Grid>
     );
   }
