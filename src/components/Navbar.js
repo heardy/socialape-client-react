c08c2cd61cc1d423085fb40946e8ccc8a099bd97
Kevin Heard <kev@kevinheard.com>
2020-05-01 21:43:22 +1000

    navbar buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import MyButton from '../util/MyButton';
 
 // MUI stuff
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 
+//Icon
+import AddIcon from '@material-ui/icons/Add';
+import HomeIcon from '@material-ui/icons/Home';
+import Notifications from '@material-ui/icons/Notifications';
+
 export class Navbar extends Component {
   render() {
+    const { authenticated } = this.props;
     return (
       <AppBar>
         <Toolbar className="nav-container">
-          <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/login">Login</Button>
-          <Button color="inherit" component={Link} to="/signup">signup</Button>
+          {authenticated ? (
+            <Fragment>
+              <MyButton tip="Post a scream!">
+                <AddIcon />
+              </MyButton>
+              <Link to="/">
+                <MyButton tip="Home">
+                  <HomeIcon />
+                </MyButton>
+              </Link>
+              <MyButton tip="Notifications">
+                <Notifications />
+              </MyButton>
+            </Fragment>
+          ): (
+            <Fragment>
+              <Button color="inherit" component={Link} to="/">Home</Button>
+              <Button color="inherit" component={Link} to="/login">Login</Button>
+              <Button color="inherit" component={Link} to="/signup">signup</Button>
+            </Fragment>
+          )}
         </Toolbar>
       </AppBar>
     )
   }
 }
 
-export default Navbar
+Navbar.propTypes = {
+  authenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated
+});
+
+export default connect(mapStateToProps)(Navbar);
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,14 +4,13 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import EditDetails from './EditDetails';
+import MyButton from '../util/MyButton';
 
 // MUI stuff
 import Button from '@material-ui/core/Button';
 import MuiLink from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
 
 // Icons
 import LocationOn from '@material-ui/icons/LocationOn';
@@ -105,11 +104,9 @@ class Profile extends Component {
           <div className="image-wrapper">
             <img src={imageUrl} alt="profile" className="profile-image" />
             <input type="file" id="imageInput" hidden="hidden" onChange={this.handleImageChange} />
-            <Tooltip title="Edit profile picture" placement="top">
-              <IconButton onClick={this.handleEditPicture} className="button">
-                <EditIcon color="primary" />
-              </IconButton>
-            </Tooltip>
+            <MyButton tip="Edit profile picture" onClick={this.handleEditPicture} btnClassName="button">
+              <EditIcon color="primary" />
+            </MyButton>
           </div>
           <hr />
           <div className="profile-details">
@@ -137,11 +134,9 @@ class Profile extends Component {
             <CalendarToday color="primary" />{' '}
             <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
           </div>
-          <Tooltip title="Logout" placement="top">
-            <IconButton onClick={this.handleLogout}>
-              <KeyboardReturn color="primary" />
-            </IconButton>
-          </Tooltip>
+          <MyButton tip="Logout" onClick={this.handleLogout}>
+            <KeyboardReturn color="primary" />
+          </MyButton>
           <EditDetails />
         </div>
       </Paper>
