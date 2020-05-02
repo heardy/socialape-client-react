31874a5fa451a5a1633316c0ecc9248b43a1dfba
Kevin Heard <kevin.heard@travelctm.com>
2020-05-02 14:23:28 +1000

    post scream component

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -1,4 +1,14 @@
-import { SET_SCREAMS, LOADING_DATA, LIKE_SCREAM, UNLIKE_SCREAM, LOADING_USER, DELETE_SCREAM } from '../types';
+import {
+  SET_SCREAMS,
+  LOADING_DATA,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+  DELETE_SCREAM,
+  LOADING_UI,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  POST_SCREAM
+} from '../types';
 import axios from 'axios';
 
 export const getScreams = () => (dispatch) => {
@@ -17,6 +27,24 @@ export const getScreams = () => (dispatch) => {
       });
     });
 };
+
+export const postScream = (newScream) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  axios.post('/scream', newScream)
+    .then(res => {
+      dispatch({
+        type: POST_SCREAM,
+        payload: res.data
+      });
+      dispatch({ type: CLEAR_ERRORS });
+    })
+    .catch(err => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data
+      })
+    });
+}
 
 export const likeScream = (screamId) => (dispatch) => {
   axios.get(`/scream/${screamId}/like`)
